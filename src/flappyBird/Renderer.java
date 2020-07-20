9a7cae5126353385fb583c3f4f0b99595cef07b2
zijian-chen96 <47934552+zijian-chen96@users.noreply.github.com>
2020-07-20 13:12:34 -0700

    Version 1.0

---
+++ src/flappyBird/Renderer.java
@@ -0,0 +1,19 @@
+package flappyBird;
+
+import java.awt.Graphics;
+import java.awt.event.ComponentListener;
+
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		FlappyBird.flappyBird.repaint(g);
+	}
+	
+}
